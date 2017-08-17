e87e69185b2f202f8a170ecd2f162e487f498470
Curtis Schlak <curtis.schlak@theironyard.com>
2017-08-17 15:11:14 -0700

    Empty not blackjack project.

---
+++ src/main/java/com/theironyard/not/blackjack/NotBlackjackApplication.java
@@ -0,0 +1,12 @@
+package com.theironyard.not.blackjack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotBlackjackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotBlackjackApplication.class, args);
+	}
+}
---
+++ src/test/java/com/theironyard/not/blackjack/NotBlackjackApplicationTests.java
@@ -0,0 +1,16 @@
+package com.theironyard.not.blackjack;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NotBlackjackApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
